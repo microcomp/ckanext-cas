503e869b3238a88a5e146e345d65557ac83d50b0
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-05-18 18:33:14 +0200

    audit

--- ckanext/cas/plugin.py
+++ ckanext/cas/plugin.py
@@ -133,162 +133,6 @@ class CasPlugin(plugins.SingletonPlugin):
         self.ckan_url = config.get('ckan.site_url', None)
         log.info('cas url: %s', self.cas_url)
 
-    def _identify(self):
-        log.info('identify')
-        c = toolkit.c
-        log.info('pylons session type: %s', type(pylons.session))
-        log.info('pylons session content: %s', pylons.session)
-        #log.info('pylons session ops: %s', dir(session))
-        environ = toolkit.request.environ
-        #log.info('c ops: %s', dir(toolkit.c))
-        #log.info('request ops: %s', dir(toolkit.request))
-        log.info('environ keys: %s', environ.keys())
-        #log.info('environ beaker get session: %s', environ['beaker.get_session'])
-        #log.info('environ beaker get session ops: %s', dir(environ['beaker.get_session']))
-        #session = environ['beaker.get_session']()
-        #log.info('session type: %s', type(environ['beaker.get_session']()))
-        #log.info('session: %s', environ['beaker.get_session']())
-        #actorid = session.get('actorid', None)
-        #log.info('session actorid: %s', actorid)
-        #log.info('environ beaker session: %s', environ['beaker.session'])
-        #log.info('beaker session type: %s', type(environ['beaker.session']))
-        #log.info('session: %s', environ['beaker.session'])
-        #log.info('session ops: %s', dir(environ['beaker.session']))
-        #session = environ['beaker.session']
-        user = environ.get('REMOTE_USER', '')
-        log.info('environ user %s', user)
-        log.info('c.user: %s', c.user)
-        if user:
-            who = environ.get("repoze.who", {})
-            log.info('repoze.who content: %s', who)
-            identity = environ.get("repoze.who.identity", {})
-            user_data = identity.get("attributes", {})
-            user_id = identity.get("repoze.who.userid")
-            actor_id = identity.get("repoze.who.actorid", None)
-            log.info('attributes: %s', user_data)
-            log.info('repoze.who user id : %s', user_id)
-            log.info('repoze.who actor id : %s', actor_id)
-            
-            if not (user_data or user_id):
-                log.info("redirect to logged_out")
-                delete_cookies()
-                h.redirect_to(controller='user', action='logged_out')
-            
-            if not user_data:
-                c.userobj = model.User.get(user_id)
-            else:
-                spr_roles = user_data.get('SPR.Roles','')
-                #delegation_type = user_data.get('DelegationType', [-1])[0]
-                subject_identity_type = int(user_data.get('Subject.IdentityType', [-1])[0])
-                log.info('subject id type: %s', type(subject_identity_type))
-                log.info('subject identity type: %s', subject_identity_type)
-                subject_id = user_data['Subject.UPVSIdentityID'][0]
-                actor_id = user_data['Actor.UPVSIdentityID'][0]
-                if 'MOD-R-PO' in spr_roles and subject_identity_type==6 and actor_id!=subject_id:
-                    log.info('povinna osoba zastupuje org')
-                    identity["repoze.who.actorid"] = user_id
-                    pylons.session['ckanext-cas-actorid'] = user_id
-                    pylons.session.save()
-                    log.info('session actual content: %s', pylons.session)
-                    identity["repoze.who.userid"] = subject_id
-                    c.userobj = model.User.get(subject_id)
-                else:
-                    log.info('nepovinna osoba zastupuje org')
-                    c.userobj = model.User.get(user_id)
-                toolkit.get_action('auditlog_send')(data_dict={'event_name' : 'user_login',
-                                                               'subject' : user_data['Subject.UPVSIdentityID'][0],
-                                                               'authorized_user' : user_data['Actor.UPVSIdentityID'][0],
-                                                               'description' : 'User login to CKAN from IP',
-                                                               'object_reference' : 'UserID://' + user_data['Subject.UPVSIdentityID'][0],
-                                                               'debug_level' : 1,
-                                                               'error_code' : 0})
-                
-            if c.userobj is None:
-                log.info("creating new user")
-                # Create the user
-                spr_roles = user_data.get('SPR.Roles', [])
-                #delegation_type = user_data.get('DelegationType', -1)
-                subject_identity_type = int(user_data.get('Subject.IdentityType', [-1])[0])
-                subject_id = user_data['Subject.UPVSIdentityID'][0]
-                actor_id = user_data['Actor.UPVSIdentityID'][0]
-                if 'MOD-R-PO' in spr_roles and subject_identity_type==6 and actor_id!=subject_id:
-                    log.info('user is org')
-                    #this user will be logged in as a subject
-                    identity["repoze.who.userid"] = user_data['Subject.UPVSIdentityID'][0]
-                    data_dict = {
-                    'password': make_password(),
-                    'fullname' : user_data['Subject.FormattedName'][0],
-                    'id' : user_data['Subject.UPVSIdentityID'][0]
-                    }
-                    if user_data.get('Subject.Username', []):
-                        data_dict['name'] = user_data['Subject.Username'][0]
-                    else:
-                        data_dict['name'] = user_data['Subject.UPVSIdentityID'][0]
-                    if user_data.get('Subject.Email', []):
-                        data_dict['email'] = user_data['Subject.Email'][0]
-
-                else:
-                    log.info('user is FO')
-                    data_dict = {
-                        'password': make_password(),
-                        'fullname' : user_data['Actor.FormattedName'][0],
-                        'id' : user_data['Actor.UPVSIdentityID'][0]
-                    }
-                    if user_data.get('Actor.Username', []):
-                        data_dict['name'] = user_data['Actor.Username'][0]
-                    else:
-                        data_dict['name'] = user_data['Actor.UPVSIdentityID'][0]
-                    if user_data.get('Actor.Email', []):
-                        data_dict['email'] = user_data.get['Actor.Email'][0]
-                log.info('data for creating user: %s', data_dict)
-                # Update the user schema to allow user creation
-                user_schema = schema.default_user_schema()
-                user_schema['id'] = [toolkit.get_validator('not_empty'), unicode]
-                user_schema['name'] = [toolkit.get_validator('not_empty'), unicode]
-                user_schema['email'] = [toolkit.get_validator('ignore_missing'), unicode]
-
-                context = {'schema' : user_schema, 'ignore_auth': True}
-                user = toolkit.get_action('user_create')(context, data_dict)
-                c.userobj = model.User.get(data_dict['id'])
-            #set c.user -> CKAN logic
-            log.info('c.userobj: %s',c.userobj)
-            c.user = c.userobj.name
-
-            if user_data:
-                role = toolkit.get_action('enum_roles')()
-                spr_roles = user_data.get('SPR.Roles','')
-                subject_id = user_data['Subject.UPVSIdentityID'][0]
-                actor_id = user_data['Actor.UPVSIdentityID'][0]
-                log.info("SPR roles: %s", spr_roles)
-                pylons.session['ckanext-cas-roles'] = spr_roles
-                pylons.session.save()
-                for spr_role in spr_roles:
-                    #delegation_type = user_data.get('DelegationType', -1)
-                    subject_identity_type = int(user_data.get('Subject.IdentityType', [-1])[0])
-                    if 'MOD-R-PO' == spr_role and subject_identity_type==6 and subject_id!=actor_id:
-                        org_id = user_data['Subject.UPVSIdentityID'][0]
-                        org_name = user_data['Subject.UPVSIdentityID'][0]
-                        org_title = user_data['Subject.FormattedName'][0]
-                        self.create_organization(org_id, org_name, org_title)
-                    
-#                     if 'MOD-R-MODER' == spr_role:
-#                         group = model.Group.get(role.ROLE_MODERATOR.lower())
-#                         if not group:
-#                             group = self._create_group_help(role.ROLE_MODERATOR)
-#                         member = self.is_member(role.ROLE_MODERATOR, c.userobj.id)
-#                         if not member:
-#                             self._add_member(group.id, c.userobj.id)
-#                             
-#                     if 'MOD-R-DATA' == spr_role:
-#                         self.create_group(role.ROLE_DATA_CURATOR)
-#                         
-#                     if 'MOD-R-APP' == spr_role:
-#                         self.create_group(role.ROLE_APP_ADMIN)
-#                     
-#                     if 'MOD-R-TRANSA' == spr_role:
-#                         self.create_group(role.ROLE_SPRAVCA_TRANSFORMACII)
-        else:
-            delete_session_items()
     def _create_user(self, data_dict, role):
         keys = {}
         keys['id'] = role + '.UPVSIdentityID'
@@ -317,15 +161,14 @@ class CasPlugin(plugins.SingletonPlugin):
             toolkit.get_action('user_create')(context, user_create_dict)
         userobj = model.User.get(user_create_dict['id'])
         return userobj
-        
-            
-        
+      
     def identify(self):
         log.info('identify')
         c = toolkit.c
         log.info('pylons session type: %s', type(pylons.session))
         log.info('pylons session content: %s', pylons.session)
         environ = toolkit.request.environ
+        log.info('environ keys: %s', environ.keys())
         user = environ.get('REMOTE_USER', '')
         log.info('environ user %s', user)
         log.info('c.user: %s', c.user)
@@ -370,6 +213,22 @@ class CasPlugin(plugins.SingletonPlugin):
                     org_name = user_data.get('Subject.Username',user_data['Subject.UPVSIdentityID'])[0]
                     org_title = user_data['Subject.FormattedName'][0]
                     self.create_organization(org_id, org_name, org_title)
+                if c.user:
+                    toolkit.get_action('auditlog_send')(data_dict={'event_name' : 'user_login',
+                                                                   'subject' : user_data['Subject.UPVSIdentityID'][0],
+                                                                   'authorized_user' : user_data['Actor.UPVSIdentityID'][0],
+                                                                   'description' : 'User login to CKAN from IP {0}'.format(environ.get('REMOTE_ADDR', '')),
+                                                                   'object_reference' : 'UserID://' + user_data['Subject.UPVSIdentityID'][0],
+                                                                   'debug_level' : 2,
+                                                                   'error_code' : 0})
+                else:
+                    toolkit.get_action('auditlog_send')(data_dict={'event_name' : 'user_login',
+                                                                   'subject' : user_data['Subject.UPVSIdentityID'][0],
+                                                                   'authorized_user' : user_data['Actor.UPVSIdentityID'][0],
+                                                                   'description' : 'User login to CKAN from IP {0}'.format(environ.get('REMOTE_ADDR', '')),
+                                                                   'object_reference' : 'UserID://' + user_data['Subject.UPVSIdentityID'][0],
+                                                                   'debug_level' : 1,
+                                                                   'error_code' : 1})
         else:
             delete_session_items()
         
